649cb6e906ca713a15c584d67dbf1ba4d3a3ccbf
Amira Chaudhry <128430603+amirachaudhry@users.noreply.github.com>
2026-08-05 16:48:37 +0100

    added remove button to wardrobe items and changed css

--- frontend/components/wardrobe.jsx
+++ frontend/components/wardrobe.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import "../css/Wardrobe.css"
+import "../css/wardrobe.css"
 
-const wardrobe = () => {
+const Wardrobe = () => {
 
   const [itemName, setItemName] = useState("")
   const [wardrobeItems, setWardrobeItemsList] = useState([])
@@ -12,8 +12,14 @@ const wardrobe = () => {
     setWardrobeItemsList([...wardrobeItems, itemName])
     setItemName("")
   }
+
+  function removeItem(index) {
+    const newWardrobeItems = [...wardrobeItems]
+    newWardrobeItems.splice(index, 1)
+    setWardrobeItemsList(newWardrobeItems)
+  }
   return (
-    <div className="wardrobe-section">
+    <div className="wardrobe-component">
       <h3>My Wardrobe</h3>
       <form className="wardrobe-form" onSubmit={addItem}>
         <input className="wardrobe-input" type='text' placeholder="Enter clothing item" value={itemName} onChange={(e) => setItemName(e.target.value)} />
@@ -21,15 +27,17 @@ const wardrobe = () => {
         <button className="item-submit" type='submit'>Add Item</button>
       </form>
       <ul className="wardrobe-list">
-        {wardrobeItems.length === 0 ? <p>No items in your wardrobe</p> : null}
-        {wardrobeItems.map((item, index) => (
-          <li className="wardrobe-item" key={index}>
-            {item}
-          </li>
-        ))}
+        {wardrobeItems.length === 0 ?
+          (<p>No items in your wardrobe</p>)
+          : (wardrobeItems.map((item, index) => (
+            <li className="wardrobe-item" key={index} >
+              <span>{item}</span>
+              <button type="button" className="item-remove" onClick={() => removeItem(index)}>Remove</button>
+            </li>
+          )))}
       </ul>
     </div>
   )
 }
 
-export default wardrobe
+export default Wardrobe
