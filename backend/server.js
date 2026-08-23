789ae9053dda0b9e7af0d597aaceae54c6d6739e
Amira Chaudhry <128430603+amirachaudhry@users.noreply.github.com>
2026-08-23 16:47:16 +0100

    set up Gemini API backend integration

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 import express from 'express';
 // allows cross-origin requests from the frontend to the backend 
 import cors from "cors"
+import dotenv from "dotenv"
+import { GoogleGenAI } from "@google/genai";
+
+dotenv.config();
+const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 
 const app = express();
 
@@ -9,7 +14,11 @@ app.use(cors());
 // allows the backend to parse JSON data from the frontend
 app.use(express.json());
 
-const POST = 5000;
+const PORT = 5000;
+
+const ai = new GoogleGenAI({
+    apiKey: GEMINI_API_KEY,
+})
 
 // request contains information sent to the server
 // response contains information sent back to the client
@@ -17,6 +26,9 @@ app.get('/', (req, res) => {
     res.json({ message: "StyleMatch backend is running" })
 })
 
-app.listen(POST, () => {
-    console.log(`Server is running on port ${POST}`);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 })
+
+console.log('Gemini key loaded:', !!process.env.GEMINI_API_KEY);
