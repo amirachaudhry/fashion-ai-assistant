0de8bffa28eb3cb266034665b8eedfaa2357031b
Amira Chaudhry <128430603+amirachaudhry@users.noreply.github.com>
2026-08-23 17:36:29 +0100

    tested outfit request and started API request for outfit suggestions

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,36 @@ app.get('/', (req, res) => {
     res.json({ message: "StyleMatch backend is running" })
 })
 
+// app.post('/api/generate-outfit', async (req, res) => {
+//     const { wardrobeItems, occasion, stylePreferences } = req.body;
+
+//     console.log("Wardrobe Items:", wardrobeItems);
+//     console.log("Occasion:", occasion);
+//     console.log("Style Preferences:", stylePreferences);
+
+//     res.json({ message: "Outfit request received" })
+// })
+
+app.post('/api/generate-outfit', async (req, res) => {
+    const { wardrobeItems, occasion, stylePreferences } = req.body;
+
+    try {
+        const prompt =
+            `Generate an outfit based on ONLY these wardrobe items: ${wardrobeItems.join(', ')}
+        The occasion is: ${occasion}. 
+        The style preferences are: ${stylePreferences}. Give a short outfit suggestion and explain why it works.`;
+
+        const response = await ai.models.generateContent({
+            model: "gemini-3.6-flash",
+            contents: prompt
+        })
+        res.json({ suggestion: response.text });
+
+    } catch (error) {
+        console.error("Error generating outfit:", error);
+        res.status(500).json({ error: "Failed to generate outfit" });
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
